b771276995635571389215d0ad79108c86512668
Erik Troan <ewt@troan.org>
2014-05-18 21:10:33 -0400

    added valuehandler.go, which lay forgotten for ages

---
+++ valuehandler.go
@@ -0,0 +1,32 @@
+package kyrie
+
+import (
+	"fmt"
+	"strconv"
+)
+
+type ValueHandler interface {
+	ValueDump(a interface{}) ([]byte, error)
+	ValueLoad([]byte) (interface{}, error)
+}
+
+type StringValueHandler struct{}
+
+func (j StringValueHandler) ValueDump(a interface{}) ([]byte, error) {
+	return []byte(a.(string)), nil
+}
+
+func (j StringValueHandler) ValueLoad(val []byte) (interface{}, error) {
+	return string(val), nil
+}
+
+type IntValueHandler struct{}
+
+func (j IntValueHandler) ValueDump(a interface{}) ([]byte, error) {
+	return []byte(fmt.Sprintf("%d", a)), nil
+}
+
+func (j IntValueHandler) ValueLoad(val []byte) (interface{}, error) {
+	value, err := strconv.ParseInt(string(val), 10, 64)
+	return int(value), err
+}
